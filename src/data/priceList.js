3e1b1c5d6c668d84b844844358405d00b3af1dbf
Udeorah Frank <franklynudeorah@gmail.com>
2023-09-23 19:35:00 +0100

    create price range section

---
+++ src/data/priceList.js
@@ -0,0 +1,22 @@
+export const priceList = [
+  {
+    treatment: "Facial Treatments",
+    price: "299"
+  },
+  {
+    treatment: "SPA & Massage",
+    price: "129"
+  },
+  {
+    treatment: "Beauty Salon",
+    price: "50"
+  },
+  {
+    treatment: "Manicure & Pedicure",
+    price: "35"
+  },
+  {
+    treatment: "Face Surgery",
+    price: "999"
+  },
+]
