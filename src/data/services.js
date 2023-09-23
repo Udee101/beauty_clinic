e63597ea9c0e57c24b27a1299ded7e39047c4334
Udeorah Frank <franklynudeorah@gmail.com>
2023-09-23 15:41:50 +0100

    create services section

---
+++ src/data/services.js
@@ -0,0 +1,18 @@
+import serviceImg1 from "../assets/serviceImg1.jpg";
+import serviceImg2 from "../assets/serviceImg2.jpg";
+import serviceImg3 from "../assets/serviceImg3.jpg";
+
+export const services = [
+  {
+		title: "BODY CARE & TREATMENT",
+		img: serviceImg1,
+	},
+	{
+		title: "BEAUTY CONSULTATION",
+		img: serviceImg2,
+	},
+	{
+		title: "BEAUTY PRODUCTS",
+		img: serviceImg3,
+	},
+]
