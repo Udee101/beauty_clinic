78e49b0b5177306630d808b1f53995cd61435084
Udeorah Frank <franklynudeorah@gmail.com>
2023-09-24 17:58:59 +0100

    refactor code and create footer section

---
+++ src/data/navLinks.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    link_name: "Home",
+    url: "#home"
+  },
+  {
+    link_name: "About",
+    url: "#about"
+  },
+  {
+    link_name: "Services",
+    url: "#services"
+  },
+  {
+    link_name: "Contact",
+    url: "#contact"
+  },
+]
---
+++ src/data/socialLinks.js
@@ -0,0 +1,23 @@
+import Facebook from "../assets/Facebook.svg";
+import Instagram from "../assets/Instagram.svg";
+import Twitter from "../assets/Twitter.svg";
+import Whatsapp from "../assets/Whatsapp.svg";
+
+export const socialLinks = [
+  {
+    img: Facebook,
+    url: "https://facebook.com"
+  },
+  {
+    img: Twitter,
+    url: "https://twitter.com"
+  },
+  {
+    img: Instagram,
+    url: "https://instagram.com"
+  },
+  {
+    img: Whatsapp,
+    url: "https://web.whatsapp.com" 
+  }
+]
